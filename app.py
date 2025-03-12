8c96cd5baf989b501be7e3b72802d4575b62e51b
RiddhiSuvariya <156102995+RiddhiSuvariya@users.noreply.github.com>
2025-03-12 23:00:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -287,13 +287,10 @@ def predict_route():
 def model_accuracies():
     return jsonify(accuracies)
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
 import os
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
