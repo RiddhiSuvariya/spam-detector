ad976b0e018c044fbe7f36c87f72fe43fda6dbb0
RiddhiSuvariya <156102995+RiddhiSuvariya@users.noreply.github.com>
2025-03-13 09:38:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -216,7 +216,7 @@ def model_accuracies():
     return jsonify(accuracies)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
 
 
